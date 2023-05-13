89685377b09bfcb7ba31faac6f962d9f845210c5
Vladimir Nedoshivin <gvozd188@mail.ru>
2023-05-13 16:34:39 +0400

    Добавление модуля css

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HeaderStyles from "./styles.module.css"
 const Header =() =>
-<h1>Knightsbridge private park – элитный район Лондона в центре Москвы
+<h1 className='title'>Knightsbridge private park – элитный район Лондона в центре Москвы
 
 </h1>
+
 export default Header
