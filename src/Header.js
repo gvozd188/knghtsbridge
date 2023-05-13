72447e3bf0803c057010e5e9a06f5a155f47ba41
Vladimir Nedoshivin <gvozd188@mail.ru>
2023-05-13 14:38:40 +0400

    Создание header

---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const Header =() =>
+<h1>Knightsbridge private park – элитный район Лондона в центре Москвы
+
+</h1>
+export default Header
